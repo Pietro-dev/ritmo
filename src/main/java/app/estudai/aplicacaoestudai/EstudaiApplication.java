c478b1a70f2b83b5a3f429f149bfca7255985b55
Pietro-dev <pietro.pietromartins@gmail.com>
2026-08-27 10:16:40 -0300

    Initial commit

---
+++ src/main/java/app/estudai/aplicacaoestudai/EstudaiApplication.java
@@ -0,0 +1,13 @@
+package app.estudai.aplicacaoestudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/estudai/aplicacaoestudai/EstudaiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.estudai.aplicacaoestudai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
