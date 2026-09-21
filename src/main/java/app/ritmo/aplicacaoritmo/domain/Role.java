8544645c931d3935b65e489987f9b45a7c27e790
Pietro-dev <pietro.pietromartins@gmail.com>
2026-09-21 18:55:01 -0300

    feat: adiciona roles e status para usuarios

---
+++ src/main/java/app/ritmo/aplicacaoritmo/domain/Role.java
@@ -0,0 +1,6 @@
+package app.ritmo.aplicacaoritmo.domain;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_ESTUDANTE
+}
---
+++ src/main/java/app/ritmo/aplicacaoritmo/domain/StatusCadastro.java
@@ -0,0 +1,6 @@
+package app.ritmo.aplicacaoritmo.domain;
+
+public enum StatusCadastro {
+    ATIVO,
+    INATIVO
+}
