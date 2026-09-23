2420e96f9feb6176463482fe38fcc7056a6e3612
Pietro-dev <pietro.pietromartins@gmail.com>
2026-09-22 21:23:44 -0300

    feat: cria metodo para mapear entity em dto

--- src/main/java/app/ritmo/aplicacaoritmo/dto/DisciplinaOutputDTO.java
+++ src/main/java/app/ritmo/aplicacaoritmo/dto/DisciplinaOutputDTO.java
@@ -1,4 +1,10 @@
 package app.ritmo.aplicacaoritmo.dto;
 
+import app.ritmo.aplicacaoritmo.domain.Disciplina;
+
 public record DisciplinaOutputDTO(Long id, String nome) {
+    // metodo para mapear a entidade em DTO
+    public static DisciplinaOutputDTO fromEntity(Disciplina disciplina) {
+        return new DisciplinaOutputDTO(disciplina.getId(), disciplina.getNome());
+    }
 }
