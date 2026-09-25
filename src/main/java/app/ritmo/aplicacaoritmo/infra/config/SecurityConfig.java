e22fe1b28f9b7826564db3ddad89486f5d8b8a7f
Pietro-dev <pietro.pietromartins@gmail.com>
2026-09-25 16:23:56 -0300

    feat: adicionar handlers à cadeida de segurança

--- src/main/java/app/ritmo/aplicacaoritmo/infra/config/SecurityConfig.java
+++ src/main/java/app/ritmo/aplicacaoritmo/infra/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package app.ritmo.aplicacaoritmo.infra.config;
 
 import app.ritmo.aplicacaoritmo.domain.Role;
+import app.ritmo.aplicacaoritmo.infra.security.LoginBemSucedidoHandler;
+import app.ritmo.aplicacaoritmo.infra.security.LogoutBemSucedidoHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    // injeção dos handlers de login e logout
+    private final LoginBemSucedidoHandler loginBemSucedidoHandler;
+    private final LogoutBemSucedidoHandler logoutBemSucedidoHandler;
 
     // bean de criptografia, disponível para toda a aplicação
     @Bean
@@ -21,20 +29,22 @@ public class SecurityConfig {
 
     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/cadastro", "/login").permitAll()
-                .requestMatchers("/admin/*").hasAuthority(Role.ROLE_ADMIN.name())
+                .requestMatchers("/cadastro", "/login", "/images/**").permitAll()
+                .requestMatchers("/admin/**").hasAuthority(Role.ROLE_ADMIN.name())
                 .anyRequest()
                 .authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
                         .usernameParameter("email")
-                        .defaultSuccessUrl("/dashboard", true)
+                        // adicao do handler de login na sequencia de filtros de segurança
+                        .successHandler(loginBemSucedidoHandler)
                         .permitAll()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
+                        // adicao do handler de logout na sequencia de filtros de segurança
+                        .logoutSuccessHandler(logoutBemSucedidoHandler)
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                         .permitAll()
