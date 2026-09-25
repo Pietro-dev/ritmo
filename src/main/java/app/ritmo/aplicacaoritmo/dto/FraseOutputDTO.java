47d8acbfb686d6e359f19671da8079ef8de3958e
Pietro-dev <pietro.pietromartins@gmail.com>
2026-09-25 15:49:12 -0300

    refactor: mapeamento correto dos campos de retorno da api

--- src/main/java/app/ritmo/aplicacaoritmo/dto/FraseOutputDTO.java
+++ src/main/java/app/ritmo/aplicacaoritmo/dto/FraseOutputDTO.java
@@ -1,7 +1,10 @@
 package app.ritmo.aplicacaoritmo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record FraseOutputDTO (String frase, String autor){
-}
+public record FraseOutputDTO (
+        @JsonProperty("content")String frase,
+        @JsonProperty("author")String autor)
+{ }
